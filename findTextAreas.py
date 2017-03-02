c808613a50483c73b263120781e31e33ea24042a
xiaofengzhu <xiaofengzhu2013@u.northwestern.edu>
2017-03-02 16:04:17 -0600

    update findTextAreas.py

--- findTextAreas.py
+++ findTextAreas.py
@@ -5,6 +5,7 @@ import numpy as np
 def captch_ex(file_name, file_name_original):
     img  = cv2.imread(file_name)
     img_original = cv2.imread(file_name_original)
+    img_original_copy = cv2.imread(file_name_original)
 
     img2gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     ret, mask = cv2.threshold(img2gray, 125, 255, cv2.THRESH_BINARY)
@@ -16,7 +17,7 @@ def captch_ex(file_name, file_name_original):
             line  8 to 12  : Remove noisy portion 
     '''
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(3 , 3)) # to manipulate the orientation of dilution , large x means horizonatally dilating  more, large y means vertically dilating more 
-    dilated = cv2.dilate(new_img,kernel,iterations = 9) # dilate , more the iteration more the dilation
+    dilated = cv2.dilate(new_img,kernel,iterations = 10) # dilate , more the iteration more the dilation
     cv2.imshow('dilated' , dilated)
     cv2.waitKey()
     contours, hierarchy = cv2.findContours(dilated,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE) # get contours
@@ -31,7 +32,7 @@ def captch_ex(file_name, file_name_original):
 
         # draw rectangle around contour on original image
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,255),2)
-        cv2.rectangle(img_original,(x,y),(x+w,y+h),(255,0,255),2)
+        cv2.rectangle(img_original_copy,(x,y),(x+w,y+h),(255,0,255),2)
         #you can crop image and send to OCR  , false detected will return no text :)
         cropped = img_original[y :y +  h , x : x + w]
         path = './crop/'+file_name[file_name.rfind('/') + 1:file_name.rfind('.')]+'/'
@@ -45,17 +46,17 @@ def captch_ex(file_name, file_name_original):
     # original image with added contours 
     cv2.imshow('captcha_result' , img)
     cv2.waitKey()
-    cv2.imshow('captcha_result' , img_original)
+    cv2.imshow('captcha_result' , img_original_copy)
     cv2.waitKey()
 
 
 def main():
-    extensions = {".jpg", ".png", ".gif"} #etc
+    extensions = {".jpg", ".png"} #etc
     flowchart_data_set_path = './highlighted/'
     original_path = './resized/'
     dirs = os.listdir(flowchart_data_set_path)   
     for flowchart_image in dirs: 
-        print flowchart_image
+        # print flowchart_image
         # e.g. flowchart_image ='basic-flowchart.jpg'
         if any(flowchart_image.endswith(ext) for ext in extensions):
             file_name = flowchart_data_set_path + flowchart_image
